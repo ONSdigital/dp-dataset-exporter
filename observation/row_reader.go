891a5b248b9f967670ce0da364e9abb8f09f3246
Carl Hembrough <carl@bitwaves.co.uk>
2017-08-04 15:03:09 +0100

    Add row reader to read data from neo4j query results and return CSV rows.

---
+++ observation/row_reader.go
@@ -0,0 +1,50 @@
+package observation
+
+import (
+	bolt "github.com/johnnadratowski/golang-neo4j-bolt-driver"
+	"github.com/johnnadratowski/golang-neo4j-bolt-driver/errors"
+)
+
+//go:generate moq -out observationtest/bolt_rows.go -pkg observationtest . BoltRows
+type BoltRows bolt.Rows
+
+// Neo4JRowReader translates Neo4j rows to CSV rows.
+type BoltRowReader struct {
+	rows BoltRows
+}
+
+// NewBoltRowReader returns a new reader instace for the given bolt rows.
+func NewBoltRowReader(rows BoltRows) *BoltRowReader {
+	return &BoltRowReader{
+		rows:rows,
+	}
+}
+
+// NoDataReturnedError is returned if a Neo4j row has no data.
+var NoDataReturnedError = errors.New("No data returned in this row.")
+
+// UnrecognisedTypeError is returned if a Neo4j row does not have the expected string value.
+var UnrecognisedTypeError = errors.New("The value returned was not a string.")
+
+// Read the next row, or return io.EOF
+func (reader *BoltRowReader) Read() (string, error) {
+	data, _, err := reader.rows.NextNeo()
+	if err != nil {
+		return "", err
+	}
+
+	if len(data) < 1 {
+		return "", NoDataReturnedError
+	}
+
+	if csvRow, ok := data[0].(string); ok {
+		return csvRow, nil
+	}
+
+	return "", UnrecognisedTypeError
+}
+
+// Close the reader.
+func (reader *BoltRowReader) Close() (error) {
+	return reader.rows.Close()
+}
---
+++ observation/row_reader_test.go
@@ -0,0 +1,123 @@
+package observation_test
+
+import (
+	"github.com/ONSdigital/dp-dataset-exporter/observation/observationtest"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+	"github.com/ONSdigital/dp-dataset-exporter/observation"
+	"io"
+)
+
+func TestBoltRowReader_Read(t *testing.T) {
+
+	Convey("Given a row reader with a mock Bolt reader", t, func() {
+
+		expectedCSVRow := "the,csv,row"
+
+		mockBoltRows := &observationtest.BoltRowsMock{
+			CloseFunc: func() error {
+				return nil
+			},
+			NextNeoFunc: func() ([]interface{}, map[string]interface{}, error) {
+				return []interface{}{ expectedCSVRow }, nil, nil
+			},
+		}
+
+		rowReader := observation.NewBoltRowReader(mockBoltRows)
+
+		Convey("When read is called", func() {
+
+			row, err := rowReader.Read()
+
+			Convey("The expected csv row is returned", func() {
+				So(err, ShouldBeNil)
+				So(row, ShouldEqual, expectedCSVRow)
+			})
+		})
+	})
+}
+
+func TestBoltRowReader_ReadError(t *testing.T) {
+
+	Convey("Given a row reader with a mock Bolt reader that returns io.EOF", t, func() {
+
+		mockBoltRows := &observationtest.BoltRowsMock{
+			CloseFunc: func() error {
+				return nil
+			},
+			NextNeoFunc: func() ([]interface{}, map[string]interface{}, error) {
+				return nil, nil, io.EOF
+			},
+		}
+
+		rowReader := observation.NewBoltRowReader(mockBoltRows)
+
+		Convey("When read is called", func() {
+
+			row, err := rowReader.Read()
+
+			Convey("The error from the Bolt reader is returned", func() {
+				So(err, ShouldNotBeNil)
+				So(err, ShouldEqual, io.EOF)
+				So(row, ShouldEqual, "")
+			})
+		})
+	})
+}
+
+func TestBoltRowReader_Read_NoDataError(t *testing.T) {
+
+	Convey("Given a row reader with a mock Bolt reader that returns a row with no data.", t, func() {
+
+		mockBoltRows := &observationtest.BoltRowsMock{
+			CloseFunc: func() error {
+				return nil
+			},
+			NextNeoFunc: func() ([]interface{}, map[string]interface{}, error) {
+				return []interface{}{ }, nil, nil
+			},
+		}
+
+		rowReader := observation.NewBoltRowReader(mockBoltRows)
+
+		Convey("When read is called", func() {
+
+			row, err := rowReader.Read()
+
+			Convey("The expected error is returned", func() {
+				So(err, ShouldNotBeNil)
+				So(err, ShouldEqual, observation.NoDataReturnedError)
+				So(row, ShouldEqual, "")
+			})
+		})
+	})
+}
+
+
+func TestBoltRowReader_Read_TypeError(t *testing.T) {
+
+	Convey("Given a row reader with a mock Bolt reader that returns a row with no data.", t, func() {
+
+		mockBoltRows := &observationtest.BoltRowsMock{
+			CloseFunc: func() error {
+				return nil
+			},
+			NextNeoFunc: func() ([]interface{}, map[string]interface{}, error) {
+				return []interface{}{ 666 }, nil, nil
+			},
+		}
+
+		rowReader := observation.NewBoltRowReader(mockBoltRows)
+
+		Convey("When read is called", func() {
+
+			row, err := rowReader.Read()
+
+			Convey("The expected error is returned", func() {
+				So(err, ShouldNotBeNil)
+				So(err, ShouldEqual, observation.UnrecognisedTypeError)
+				So(row, ShouldEqual, "")
+			})
+		})
+	})
+}
