c770abf05dac5e16a6a9c4c5621f13dca28832d6
Carl Hembrough <carl@bitwaves.co.uk>
2017-08-08 15:21:16 +0100

    Add schema for csv exported event.

--- schema/schema.go
+++ schema/schema.go
@@ -17,3 +17,17 @@ var filterJobSubmittedEvent = `{
 var FilterJobSubmittedEvent *avro.Schema = &avro.Schema{
 	Definition: filterJobSubmittedEvent,
 }
+
+var csvExportedEvent = `{
+  "type": "record",
+  "name": "csv-exported",
+  "namespace": "",
+  "fields": [
+    {"name": "filter_job_id", "type": "string"}
+  ]
+}`
+
+// CSVExportedEvent the Avro schema for CSV exported messages.
+var CSVExportedEvent *avro.Schema = &avro.Schema{
+	Definition: csvExportedEvent,
+}
