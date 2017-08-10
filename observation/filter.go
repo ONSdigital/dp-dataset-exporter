1a9822e0f18c16c91b7bdc0066b415da941b3cc4
Carl Hembrough <carl@bitwaves.co.uk>
2017-08-10 15:31:34 +0100

    Add download properties to filter model.

--- observation/filter.go
+++ observation/filter.go
@@ -7,6 +7,12 @@ type Filter struct {
 	DimensionListURL string             `json:"dimension_list_url"`
 	State            string             `json:"state,omitempty"`
 	DimensionFilters []*DimensionFilter `json:"dimensions,omitempty"`
+	Downloads        *Downloads         `json:"downloads,omitempty"`
+}
+
+// Downloads represents a list of file types possible to download
+type Downloads struct {
+	CSV *DownloadItem `json:"csv,omitempty"`
 }
 
 // DimensionFilter represents an object containing a list of dimension values and the dimension name
@@ -14,4 +20,10 @@ type DimensionFilter struct {
 	Name   string   `json:"name,omitempty"`
 	URL    string   `json:"dimension_url,omitempty"`
 	Values []string `json:"values,omitempty"`
+}
+
+// DownloadItem represents an object containing information for the download item
+type DownloadItem struct {
+	Size string `json:"size,omitempty"`
+	URL  string `json:"url,omitempty"`
 }
